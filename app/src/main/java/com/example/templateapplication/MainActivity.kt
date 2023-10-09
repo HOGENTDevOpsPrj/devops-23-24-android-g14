6922ebfd61c580f22205e59678c0d1bf6a49356b
Marwane Koutar <marwane.koutar@student.hogent.be>
2023-10-09 16:13:57 +0200

    textfields and colors

--- app/src/main/java/com/example/templateapplication/MainActivity.kt
+++ app/src/main/java/com/example/templateapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.templateapplication
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,14 +8,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -24,13 +30,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -57,18 +66,52 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun createTicket() {
+    var text by rememberSaveable { mutableStateOf("") }
+
     Scaffold(
         containerColor = Color.Transparent,
-        modifier = Modifier.padding(20.dp)
+        bottomBar = {
+            BottomAppBar(
+                containerColor = Color.Transparent,
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Localized description")
+                    }
+                }
+            )
+        }
     ) { innerPadding ->
 
         Column(
             modifier = Modifier
-                .padding(innerPadding),
+                .padding(innerPadding).padding(15.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+
         ) {
             Image(painter = painterResource(id = R.drawable._15_svk_logo_met_slogan_black_01), contentDescription = null)
-
+            Spacer(modifier = Modifier.height(30.dp))
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Routenummer") },
+                placeholder = { Text("Routenummer") },
+                )
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Laadbonnummer") },
+                placeholder = { Text("Laadbonnummer") },
+                )
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Nummerplaat") },
+                placeholder = { Text("Nummerplaat") },
+                )
         }
     }
 }
