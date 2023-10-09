66b2eaaccbab3bee0af92f2956de1d0aa937734d
Lars Salembier <lars.salembier@gmail.com>
2023-10-09 21:27:56 +0200

    Eerste scherm UI is afgewerkt.

--- app/src/main/java/com/example/templateapplication/MainActivity.kt
+++ app/src/main/java/com/example/templateapplication/MainActivity.kt
@@ -1,49 +1,38 @@
 package com.example.templateapplication
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.AlertDialogDefaults.containerColor
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.templateapplication.ui.theme.TemplateApplicationTheme
@@ -63,63 +52,74 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun createTicket() {
+fun CreateTicket() {
     var text by rememberSaveable { mutableStateOf("") }
-
-    Scaffold(
-        containerColor = Color.Transparent,
-        bottomBar = {
-            BottomAppBar(
-                containerColor = Color.Transparent,
-                actions = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(Icons.Filled.Menu, contentDescription = "Localized description")
-                    }
-                }
-            )
+    Image(
+        painter = painterResource(id = R.drawable.svk_logo_zonder_slogan),
+        contentDescription = "SVK Logo",
+        modifier = Modifier.height(64.dp)
+    )
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    "Lading registreren", maxLines = 1, overflow = TextOverflow.Ellipsis
+                )
+            }
+        }, actions = {
+            IconButton(onClick = {/*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.ExitToApp, contentDescription = "Uitloggen"
+                )
+            }
+        })
+    }, floatingActionButton = {
+        LargeFloatingActionButton(
+            onClick = { },
+        ) {
+            Icon(Icons.Default.Send, "Large floating action button")
         }
-    ) { innerPadding ->
-
+    }) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(innerPadding).padding(15.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(innerPadding)
+                .padding(32.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Image(painter = painterResource(id = R.drawable._15_svk_logo_met_slogan_black_01), contentDescription = null)
-            Spacer(modifier = Modifier.height(30.dp))
+            Image(
+                painter = painterResource(id = R.drawable._15_svk_logo_met_slogan_black_01),
+                contentDescription = "SVK Logo"
+            )
             TextField(
-                modifier = Modifier.fillMaxWidth(),
                 value = text,
                 onValueChange = { text = it },
                 label = { Text("Routenummer") },
                 placeholder = { Text("Routenummer") },
-                )
+                modifier = Modifier.fillMaxWidth()
+            )
             TextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = text,
-                onValueChange = { text = it },
-                label = { Text("Laadbonnummer") },
-                placeholder = { Text("Laadbonnummer") },
-                )
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
                 value = text,
                 onValueChange = { text = it },
                 label = { Text("Nummerplaat") },
                 placeholder = { Text("Nummerplaat") },
-                )
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+                Text("Voeg foto toe")
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun createTicketPreview() {
-    TemplateApplicationTheme {
-        createTicket()
+fun CreateTicketPreview() {
+    TemplateApplicationTheme(useDarkTheme = false) {
+        CreateTicket()
     }
 }
