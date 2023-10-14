2d3233011b14a30e1e9e3f40c25203b763ca5210
Marwane Koutar <marwane.koutar@student.hogent.be>
2023-10-14 20:07:42 +0200

    navigation working + login screen to test

---
+++ app/src/main/java/com/hogent/svkapp/features/login/presentation/ui/LoginScreen.kt
@@ -0,0 +1,32 @@
+package com.hogent.svkapp.features.login.presentation.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.hogent.svkapp.main.presentation.ui.SVKLogo
+import com.hogent.svkapp.main.presentation.ui.theme.spacing
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(all = MaterialTheme.spacing.large),
+        verticalArrangement = Arrangement.spacedBy(space = MaterialTheme.spacing.large)
+    ) {
+        SVKLogo()
+        Button(onClick = { navController.navigate("createTicket") }) {
+            Text("Login")
+        }
+    }
+}
