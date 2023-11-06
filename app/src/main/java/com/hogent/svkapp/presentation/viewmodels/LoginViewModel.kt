33c0af54dbccf5589bddee4b1796b50f4fe05bc0
Lars Salembier <lars.salembier@gmail.com>
2023-11-06 14:29:01 +0100

    Fixed

--- app/src/main/java/com/hogent/svkapp/presentation/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/hogent/svkapp/presentation/viewmodels/LoginViewModel.kt
@@ -1,7 +1,5 @@
 package com.hogent.svkapp.presentation.viewmodels
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import com.hogent.svkapp.Route
@@ -12,22 +10,23 @@ import com.hogent.svkapp.Route
  * @param navController the [NavHostController] that is used to navigate to other screens.
  */
 class LoginViewModel(private val navController: NavHostController) : ViewModel() {
-    private var _userEmail = mutableStateOf(value="")
-    val userEmail: State<String> get() = _userEmail
+//    private var _userEmail = mutableStateOf(value = "")
+//
+//    /**
+//     * The email of the user.
+//     */
+//    val userEmail: State<String> get() = _userEmail
+//
+//    private var _password = mutableStateOf(value = "")
+//
+//    /**
+//     * The password of the user.
+//     */
+//    val password: State<String> get() = _password
 
-    private var _password = mutableStateOf(value="")
-    val password: State<String> get() = _password
-
-    fun onUserEmailChange(userEmail: String) {
-        _userEmail.value = userEmail
-        // validatie via LoginValidator
-    }
-
-    fun onPasswordChange(password: String) {
-        _password.value = password
-        // validatie / encryptie ??
-    }
-
+    /**
+     * Called when login button is clicked.
+     */
     fun onLogin() {
         navController.navigate(Route.Main.name)
     }
