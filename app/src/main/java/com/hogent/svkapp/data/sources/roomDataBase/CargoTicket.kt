1ed6624388334f59d964cb3aa42d95e18bb7050f
Thomas Gielen <thomas.gielen@student.hogent.be>
2023-11-28 14:55:52 +0100

    created LadingApiService

--- app/src/main/java/com/hogent/svkapp/data/sources/roomDataBase/CargoTicket.kt
+++ app/src/main/java/com/hogent/svkapp/data/sources/roomDataBase/CargoTicket.kt
@@ -3,15 +3,11 @@ package com.hogent.svkapp.data.sources.roomDataBase
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.hogent.svkapp.domain.entities.Image
-import com.hogent.svkapp.domain.entities.ImageCollection
-import com.hogent.svkapp.domain.entities.LicensePlate
-import com.hogent.svkapp.domain.entities.RouteNumberCollection
 
 @Entity(tableName = "cargoTicket")
 data class CargoTicket(
-    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
-    @ColumnInfo(name="route_numbers") val routeNumbers: RouteNumberCollection,
-    @ColumnInfo(name="licence_plate") val licensePlate: LicensePlate,
-    @ColumnInfo(name="images") val images: ImageCollection,
+    @PrimaryKey(autoGenerate = true) val id: String = "",
+    @ColumnInfo(name="route_numbers") val routeNumbers: List<String>,
+    @ColumnInfo(name="licence_plate") val licensePlate: String,
+    @ColumnInfo(name="images") val images: List<String>,
 )
