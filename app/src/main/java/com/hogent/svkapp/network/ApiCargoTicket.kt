e3a528451b0e2c091725df780d81daa7cdee098b
Marwane Koutar <marwane.koutar@student.hogent.be>
2023-12-10 20:23:44 +0100

    bug fixes (azure blob working, datetime fixed)

--- app/src/main/java/com/hogent/svkapp/network/ApiCargoTicket.kt
+++ app/src/main/java/com/hogent/svkapp/network/ApiCargoTicket.kt
@@ -1,7 +1,11 @@
 package com.hogent.svkapp.network
 
+import android.util.Log
 import com.hogent.svkapp.domain.entities.CargoTicket
 import kotlinx.serialization.Serializable
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @Serializable
 data class ApiCargoTicket (
@@ -14,8 +18,6 @@ data class ApiCargoTicket (
 )
 
 class CargoTicketConverter {
-
-
     companion object {
         fun convertToApiCargoTicket(cargoTicket: CargoTicket): ApiCargoTicket {
             return ApiCargoTicket(
@@ -24,12 +26,16 @@ class CargoTicketConverter {
                 licensePlate = cargoTicket.licensePlate.value,
                 imageUrls = cargoTicket.images.value.map {
                     val fotoUploader = AzureBlobUploader("https://svkstorageg14.blob.core.windows.net/");
-                    val sasToken= "?sv=2022-11-02&ss=b&srt=co&sp=rwdlaciytfx&se=2024-01-29T23:12:27Z&st=2023-12-05T15" +
-                            ":12:27Z&spr=https&sig=yGX%2FMbbmyTAu4ACKxf8MK2RB1GJiR1wyPv%2FXyRz0ReE%3D"
+                    val sasToken =
+                        "?sv=2022-11-02&ss=b&srt=co&sp=rwdlaciytfx&se=2024-01-29T23:12:27Z&st=2023-12-05T15" +
+                                ":12:27Z&spr=https&sig=yGX%2FMbbmyTAu4ACKxf8MK2RB1GJiR1wyPv%2FXyRz0ReE%3D"
                     fotoUploader.uploadImage(it.id, sasToken, it.bitmap)
                 },
-                freightLoaderId = "eb9ac197-23c5-42b0-9641-a5fb4fa92336",
-                registrationDateTime = "2023-12-01T12:00:00"
+                freightLoaderId = "5e548527-426b-43d7-b90a-ffee739e1900",
+                registrationDateTime = LocalDateTime.now(ZoneId.systemDefault()).format(
+                    DateTimeFormatter.ofPattern
+                        ("yyyy-MM-dd'T'HH:mm:ss")
+                )
             )
         }
     }
--- app/src/main/java/com/hogent/svkapp/network/FotoBlobStorageService.kt
+++ app/src/main/java/com/hogent/svkapp/network/FotoBlobStorageService.kt
@@ -1,7 +1,10 @@
 package com.hogent.svkapp.network
 
 import android.graphics.Bitmap
+import android.util.Log
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
@@ -11,54 +14,69 @@ import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Url
 import java.io.ByteArrayOutputStream
 
 interface FotoBlobStorageService {
 
     @Headers(
-        "Content-Type: image/jpeg",
         "x-ms-blob-type: BlockBlob"
     )
-    @PUT("images/{blobName}/{sasToken}")
+    @PUT
     fun uploadImage(
-        @Path("blobName") blobName: String,
-        @Query("sasToken") sasToken: String,
+        @Url url: String,
         @Body body: RequestBody
     ): Call<Void>
 }
 
+
+
 class AzureBlobUploader(private val baseUrl: String) {
+
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .build()
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
         .addConverterFactory(ScalarsConverterFactory.create())
+        .client(okHttpClient)
         .build()
 
     private val service: FotoBlobStorageService = retrofit.create(FotoBlobStorageService::class.java)
 
-    fun uploadImage( blobName: String, sasToken: String, bitmap: Bitmap): String {
+    fun uploadImage(blobName: String, sasToken: String, bitmap: Bitmap): String {
         val imageData = convertBitmapToRequestBody(bitmap)
 
-        val call = service.uploadImage(blobName, sasToken, imageData)
+        // Construct the full URL with query parameters
+        val fullUrl = "${baseUrl}images/$blobName$sasToken"
+
+        val call = service.uploadImage(fullUrl, imageData)
         call.enqueue(object : Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 // Handle successful response
+                Log.d("SuccessImage", "SuccessImage")
+                Log.d("Response Body", response.toString())
             }
 
             override fun onFailure(call: Call<Void>, t: Throwable) {
                 // Handle failure
+                Log.d("ErrorImage", "ErrorImage")
             }
         })
-        return "${baseUrl}/images/${blobName}";
+        return fullUrl
     }
 
     private fun convertBitmapToRequestBody(bitmap: Bitmap): RequestBody {
         val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
         val imageData = outputStream.toByteArray()
-        return imageData.toRequestBody("image/jpeg".toMediaTypeOrNull())
+        return RequestBody.create("image/png".toMediaTypeOrNull(), imageData)
     }
 }
+
+
